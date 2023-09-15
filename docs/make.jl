0737e3c96c65241fef0fdab089284ce092b1693e
cgarling <chris.t.garling@gmail.com>
2023-09-15 15:44:08 -0400

    allow Documenter.jl v1

--- docs/make.jl
+++ docs/make.jl
@@ -10,8 +10,9 @@ makedocs(
     modules = [Cosmology],
     format = Documenter.HTML(;prettyurls = get(ENV, "CI", nothing) == "true"),
     authors = "Chris Garling",
-    pages = ["guide.md","types.md","public_methods.md","integrated_packages.md","private_methods.md","constants.md","index.md"],#,"api.md"],
-    doctest=true
+    pages = ["guide.md","types.md","public_methods.md","integrated_packages.md","private_methods.md","constants.md","index.md"],
+    doctest=true,
+    warnonly = [:missing_docs] # Do not error if we are missing a docstring in the module
 )
 
 deploydocs(;
